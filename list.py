a410307834d79e673e0d45fa563a96e2214c2bf3
Sanskar Sarak <sanskarsarak91@gmail.com>
2026-07-21 23:39:20 +0530

    Add files via upload
    this is the student helpfull all List Function in this python file.

---
+++ list.py
@@ -0,0 +1,49 @@
+
+# # this is list functions
+
+# print("List=",list)
+
+# this is the listand it is identifying [] and list are add in collection of items in different data types. 
+a=["sanskar", 12, 23.5, True, "sahil"]
+
+b=["123", "sanskar12"]
+#print(a[1:4]) used for 1 to 4 show inside element.
+
+#print(a.count(12))  ----used for count same element in the list. (don't count all element)
+
+# a.insert(2,"dada")  ----this is used for add element in choose your position.
+
+# a.pop(1) ---used for Delete element but take a only allow  element indexing number. dont take element name.
+
+#a.remove(True) ----pop and remove are the two different concept but result one. remove achieve in element name. not a used Indexing number.
+
+#a.pop() ---- without position work---by default last element are delete.
+
+#a.extend(b)  
+#b.extend(a)-----this is used for two diffrent lists in add one list .
+
+# c=a.copy() -- copy other list element and show second copy of first list.
+
+#a.clear() #---for all list element clear
+
+
+# c=["sanskar", "samadhan", "soham"]
+# c.sort() 
+# #list.sort(reverse=True) -----this is the sort function. used in small to large character.
+ 
+# a.reverse()  --- reverse list elements
+
+
+# print(a)
+
+# list=[12,32,1,2,3,4,5,5,4,3,3,["sanskar", "soham", 'samadhan', "sanjay"]] -----this is called nested list or list inside list. list under multiple list add 
+
+# list=['sanskar','sahil','dhanu', 'dada', 'siddharth']
+# a=[word for word in list if word.endswith("l")]
+# print(a)
+
+# a=['sanskar','ankush','rahul'] 
+# n1,n2,n3=a
+# print(n1)
+# print(n2)
+# print(n3)------jevdhe list madhe element aastil tevdhe khali n's gheave lagtil.varti list element 3 aastil tr khali n1,n2,n3 pratekasathi vegle ghave lagtat.
---
+++ practice.py
@@ -0,0 +1,70 @@
+#list=['apple',"banana", "mango", "Orange", "kiwi"]
+
+# print(list[0])
+# print(list[2])
+# print(list[4])
+
+#print(list[1:4])  ----this is the slicing 
+
+#a=[10,20,10,30,40,10,10,10]
+# print(a.count(10))----count element in this list how many time.
+
+#a=["ram", "Shyam", "Mohan"]
+# a.insert(1,"Sanskar")
+# a.append("Pune")
+# a.pop(1)
+# a.remove("Shyam")
+# a.clear()
+
+#a=["ram", "Shyam", "Mohan"]
+# b=a.copy()
+# print(b)
+
+# a=[1,2,3]
+# b=[4,5,6]
+# c=a+b
+# print(c)----this is two combine lists in create 1 list
+
+# a=[10,20,30,40]
+# a.reverse()
+# print(a)----this is the list reverse operation.
+
+# a=[25,10,5,90,12]
+# a.sort()
+# print(a) -----this is the __assending list__ sort means small to large showing numbers in the list.
+
+# a=[25,10,5,90,12]
+# a.sort(reverse=True)
+# print(a) ----this is the __Dessending list sort means print large to small numbers in the list.
+
+# a=[10,20,30,[30,50,10,[89,56,21]]]
+# list=[10,20,30,[40,50,60,70,80],90]
+# print(type(list))
+# print(list[3][1]) this is the nestend list.
+
+# print(len(a)) this function check list lenght
+
+# a=[12,2,3,4,5,6,4,3,3]
+# if 3 in a:
+#     print("yes")
+# else:
+#     print("no") ----this is used in condition 
+
+# a=[12,32,13,54]
+# # a[-2]="Black"
+# # a.append(10)
+# # a.append(20)
+# # a.append(30)
+# a.extend([10,20,89, "sanskar"])
+# print(a)
+
+a=[2,4,6,8,10]
+if a==2:
+    print("correct", a)
+else:
+    print("flase", a)
+
+
+
+
+ 
